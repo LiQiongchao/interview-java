362022e3011ae65c2e5b3672460d85cd7b933ec2
liqiongchao-home <liqiongchao2016@gmail.com>
2020-05-25 23:38:48 +0800

    mmap可重入锁

---
+++ src/main/java/com/newlycode/interview/SafeListDemo.java
@@ -0,0 +1,15 @@
+package com.newlycode.interview;
+
+import java.util.Collections;
+
+/**
+ * @Author: LiQiongchao
+ * @Date: 2020/5/25 23:17
+ */
+public class SafeListDemo {
+
+    public static void main(String[] args) {
+
+    }
+
+}
