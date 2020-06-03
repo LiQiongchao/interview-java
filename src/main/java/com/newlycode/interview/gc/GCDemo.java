b38b98f1973b2ffbbb53b99cf27c6f8f5349cfb7
liqiongchao-home <liqiongchao2016@gmail.com>
2020-06-03 19:37:22 +0800

    jinfo -flag MaxTenuringThreshold 16920 查看垃圾最大年龄。

--- src/main/java/com/newlycode/interview/gc/GCDemo.java
+++ src/main/java/com/newlycode/interview/gc/GCDemo.java
@@ -1,16 +1,21 @@
 package com.newlycode.interview.gc;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * VM: -Xms10m -Xmx10m -XX:+PrintGCDetails
  * 放开bype数组，会oom，会打印gc日志，否则只打印堆栈信息，不打印GC与full GC的日志。
+ *
+ * - jinfo -flag MaxTenuringThreshold 16920 查看垃圾最大年龄。
  * @Author: LiQiongchao
  * @Date: 2020/6/2 22:34
  */
 public class GCDemo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("hello GC");
-        Byte[] bytes = new Byte[50 * 1024 * 1024];
+        TimeUnit.SECONDS.sleep(Integer.MAX_VALUE);
+//        Byte[] bytes = new Byte[50 * 1024 * 1024];
         System.out.println("==========================");
     }
 }
