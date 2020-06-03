74365fa09e44c61099dfc6e898f3aa4a9d43f049
liqiongchao-home <liqiongchao2016@gmail.com>
2020-06-03 20:37:32 +0800

    虚引用

---
+++ src/main/java/com/newlycode/interview/gc/reference/PhantomReferenceDemo.java
@@ -0,0 +1,34 @@
+package com.newlycode.interview.gc.reference;
+
+import java.lang.ref.PhantomReference;
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.WeakReference;
+
+/**
+ * 虚引用
+ *  与不存在一样，无法使用，只能在对象回收时做一此处理。
+ *
+ * @Author: LiQiongchao
+ * @Date: 2020/6/3 19:41
+ */
+public class PhantomReferenceDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+        Object object1 = new Object();
+        ReferenceQueue<Object> referenceQueue = new ReferenceQueue<>();
+        PhantomReference<Object> phantomReference = new PhantomReference<>(object1, referenceQueue);
+
+        System.out.println(object1);
+        System.out.println(phantomReference.get());
+        System.out.println(referenceQueue.poll());
+
+        System.out.println("=====================");
+        object1 = null;
+        System.gc();
+        Thread.sleep(500L);
+
+        System.out.println(object1);
+        System.out.println(phantomReference.get());
+        System.out.println(referenceQueue.poll());
+    }
+}
